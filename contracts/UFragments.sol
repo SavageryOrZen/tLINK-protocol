6ac3689b91237a5ba100f49d098fdcda45d38435
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-08-17 09:29:10 -0300

    initial supply

--- contracts/UFragments.sol
+++ contracts/UFragments.sol
@@ -60,7 +60,7 @@ contract UFragments is ERC20Detailed, Ownable {
 
     uint256 private constant DECIMALS = 9;
     uint256 private constant MAX_UINT256 = ~uint256(0);
-    uint256 private constant INITIAL_FRAGMENTS_SUPPLY = 380 * 10**6 * 10**DECIMALS;
+    uint256 private constant INITIAL_FRAGMENTS_SUPPLY = 35 * 10**6 * 10**DECIMALS;
 
     // TOTAL_GONS is a multiple of INITIAL_FRAGMENTS_SUPPLY so that _gonsPerFragment is an integer.
     // Use the highest value that fits in a uint256 for max granularity.
